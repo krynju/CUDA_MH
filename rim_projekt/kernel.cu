a44f270082138f931092ebdc72046f0c2e3d1de0
Krystian Guliński <gulinski.krystian@gmail.com>
2020-12-13 21:07:02 +0100

    sequential

--- rim_projekt/kernel.cu
+++ rim_projekt/kernel.cu
@@ -1,8 +1,14 @@
-
+﻿#define _USE_MATH_DEFINES
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include "helper_cuda.h"
+#include <chrono>
 
 #include <stdio.h>
+#include <random>
+#include <tuple>
+#include <math.h>
+
 
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
@@ -12,12 +18,112 @@ __global__ void addKernel(int *c, const int *a, const int *b)
     c[i] = a[i] + b[i];
 }
 
+
+
+double norm_pdf(double mean, float std, float x) {
+    const double a = 1 / sqrt(2 * M_PI);
+    return a / std * exp(-0.5 * pow(((x - mean) / std), 2));
+}
+
+double ff(float x) {
+    x = x / 100.0;
+    return norm_pdf(0.0, 1.0, x) + norm_pdf(3.0, 1.0, x) + norm_pdf(6.0, 1.0, x);
+}
+
+std::tuple<float, double, double> _gen_candidate(float xt, float std, double f_xt, double (*f)(float), std::mt19937 gen) {
+    std::normal_distribution<> g(xt, std);
+    float xc = g(gen);
+    double f_xc = f(xc);
+    double a = f_xc / f_xt;
+    return std::tuple<float, double, double>(xc, a, f_xc);
+}
+
+void _generate_loop(float* x, float xt, float std, int start_range, int end_range, double (*f)(float), std::mt19937 gen) {
+    double f_xt = f(xt);
+    std::tuple<float, double, double> res;
+    std::uniform_real<double> uni(0.0, 1.0);
+    for (int t = start_range; t < end_range; t++) {
+        res = _gen_candidate(xt, std, f_xt, f, gen);
+        float u = uni(gen);
+        if (u <= std::get<1>(res)) {
+            xt = std::get<0>(res);
+            f_xt = std::get<2>(res);
+        }
+        x[t] = xt;
+    }
+}
+
+std::tuple<float, float> _burn_loop(float* x, float xt, float std, int burn_N, double (*f)(float), std::mt19937 gen) {
+    double f_xt = f(xt);
+    std::tuple<float, double, double> res;
+    std::uniform_real<double> uni(0, 1);
+    const float target = 0.3;
+    int accepted = 0;
+    for (int t = 0; t < burn_N; t++) {
+        res = _gen_candidate(xt, std, f_xt, f, gen);
+        float u = uni(gen);
+        if (u <= std::get<1>(res)) {
+            xt = std::get<0>(res);
+            f_xt = std::get<2>(res);
+            accepted += 1;
+        }
+        float temp = accepted /( t + 1.0);
+        float reg = 1/std*1000.0 * (temp - target);
+        if (!(reg < 0 && abs(reg) > std)) {
+            std += reg;
+        }
+    }
+    return std::tuple<float, float>(xt, std);
+}
+
+float* mh_v1(float x0, int N, int burn_N, double (*f)(float), int seed) {
+    float* x = (float*) malloc(sizeof(float) * N);
+    float xt = x0;
+    float std = 1.0;
+
+    std::mt19937 gen(seed);
+
+    std::tuple<float, float> res;
+    res = _burn_loop(x, xt, std, burn_N, f, gen);
+    printf("std = %f", std::get<1>(res));
+    _generate_loop(x, std::get<0>(res), std::get<1>(res), 0, N, f, gen);
+    
+    return x;
+}
+
+
+
+
+
+
 int main()
 {
     const int arraySize = 5;
     const int a[arraySize] = { 1, 2, 3, 4, 5 };
     const int b[arraySize] = { 10, 20, 30, 40, 50 };
     int c[arraySize] = { 0 };
+
+
+    int N = 100000;
+
+    typedef std::chrono::high_resolution_clock Clock;
+    Clock::time_point t0 = Clock::now();
+    float* x = mh_v1(0.0, N, 100000, ff, 42141241);
+    Clock::time_point t1 = Clock::now();
+    auto d = std::chrono::duration_cast<std::chrono::milliseconds>(t1-t0);
+    printf("%d \n", d.count());
+
+    FILE* f = fopen("result.csv", "w");
+    for (int i = 0; i < N; i++) {
+        fprintf(f, "%f\n", x[i]);
+    }
+    fclose(f);
+
+    //FILE* f = fopen("result.csv", "w");
+    //for (int i = -500; i < 1200; i++) {
+    //    fprintf(f, "%f\n", ff(1.0*i));
+    //}
+    //fclose(f);
 
     // Add vectors in parallel.
     cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
@@ -55,7 +161,7 @@ cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
         goto Error;
     }
 
-    // Allocate GPU buffers for three vectors (two input, one output)    .
+    // Allocate GPU buffers for three vectors (two input, one output).
     cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMalloc failed!");
